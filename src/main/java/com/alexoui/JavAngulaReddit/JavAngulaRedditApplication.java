d88750207fb61d71b58a523e5297495e9bfc07b0
OUILLON Alex <alex.ouillon@outlook.fr>
2022-04-25 23:24:51 +0200

    Initializing models

---
+++ src/main/java/com/alexoui/JavAngulaReddit/JavAngulaRedditApplication.java
@@ -0,0 +1,13 @@
+package com.alexoui.JavAngulaReddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavAngulaRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavAngulaRedditApplication.class, args);
+	}
+
+}
